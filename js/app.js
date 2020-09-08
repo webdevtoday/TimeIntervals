92fe3bee4d8c8cd80b57e1f4c212725fa9c97e69
webdevtoday <coderwd@gmail.com>
2020-09-08 22:54:35 +0300

    Add Process object to app.js

--- js/app.js
+++ js/app.js
@@ -3,11 +3,39 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 let Processes = {
+    list: [
+        {
+            id: 1,
+            name: "Тестовый сайт",
+            description: "Хочу отслеживать сколько чистого времени занимает разработка сайта.",
+            timeIntervals: [
+                {
+                    id: 1,
+                    name: "Работа с HTML",
+                    description: "Добавил начальную разметку и начал работу над шапкой",
+                    from: 0,
+                    to: 1,
+                },
+                {
+                    id: 2,
+                    name: "Шапка",
+                    description: "Залип в шапке, разметил элементы шапки и наполнил их классами.",
+                    from: 0,
+                    to: 1,
+                },
+            ],
+            time_created: 0,
+            time_modified: 0,
+            total: {
+                work_time: 0,
+                all_time: 0,
+            },
+        },
+    ],
     get: {
         list: function() { return "Processes.get.list();"; },
-        process: function(process_name) {
-            // process name || process id
-            return `Processes.get.process(${process_name})`;
+        process: function(process_id) {
+            return `Processes.get.process(${process_id})`;
         },
     },
     set: {
@@ -15,4 +43,48 @@ let Processes = {
             return "Processes.set.process()";
         },
     }
+};
+
+// Пример обьекта процесса с временными отрезками
+// {
+//     name: "Раны в ДМ",
+//     description: "Раны в ДМ за понедельник",
+//     timeIntervals: [
+//         {
+//             id: 1,
+//             name: "Первый ран",
+//             description: "Описание этого рана. Что-то произошло и тд.",
+//             from: 0,
+//             to: 1,
+//         },
+//         {
+//             id: 2,
+//             name: "Второй ран",
+//             description: "Описание этого рана. Что-то произошло и тд.",
+//             from: 0,
+//             to: 1,
+//         },
+//     ],
+//     time_created: 0,
+//     time_modified: 0,
+//     total: {
+//         work_time: 0,
+//         all_time: 0,
+//     },
+// }
+
+let Process = {
+    create: function() {
+        return {
+            name: "Раны в ДМ",
+            description: "Раны в ДМ за понедельник",
+            timeIntervals: [],
+            time_created: 0,
+            time_modified: 0,
+            total: {
+                work_time: 0,
+                all_time: 0,
+            },
+        };
+    },
 };

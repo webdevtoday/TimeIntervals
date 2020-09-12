bdf91466a9cc5a7ae285b4416c9a05df1b657bff
webdevtoday <coderwd@gmail.com>
2020-09-12 16:28:37 +0300

    Add .prettierrc.json. Formatting code in project.

--- js/app.js
+++ js/app.js
@@ -1,48 +1,50 @@
-document.addEventListener("DOMContentLoaded", function () {
-
-});
+document.addEventListener('DOMContentLoaded', function () {});
 
 let Processes = {
-    list: [
+  list: [
+    {
+      id: 1,
+      name: 'Тестовый сайт',
+      description:
+        'Хочу отслеживать сколько чистого времени занимает разработка сайта.',
+      timeIntervals: [
         {
-            id: 1,
-            name: "Тестовый сайт",
-            description: "Хочу отслеживать сколько чистого времени занимает разработка сайта.",
-            timeIntervals: [
-                {
-                    id: 1,
-                    name: "Работа с HTML",
-                    description: "Добавил начальную разметку и начал работу над шапкой",
-                    from: 0,
-                    to: 1,
-                },
-                {
-                    id: 2,
-                    name: "Шапка",
-                    description: "Залип в шапке, разметил элементы шапки и наполнил их классами.",
-                    from: 0,
-                    to: 1,
-                },
-            ],
-            time_created: 0,
-            time_modified: 0,
-            total: {
-                work_time: 0,
-                all_time: 0,
-            },
+          id: 1,
+          name: 'Работа с HTML',
+          description: 'Добавил начальную разметку и начал работу над шапкой',
+          from: 0,
+          to: 1,
         },
-    ],
-    get: {
-        list: function() { return "Processes.get.list();"; },
-        process: function(process_id) {
-            return `Processes.get.process(${process_id})`;
+        {
+          id: 2,
+          name: 'Шапка',
+          description:
+            'Залип в шапке, разметил элементы шапки и наполнил их классами.',
+          from: 0,
+          to: 1,
         },
+      ],
+      time_created: 0,
+      time_modified: 0,
+      total: {
+        work_time: 0,
+        all_time: 0,
+      },
     },
-    set: {
-        process: function() {
-            return "Processes.set.process()";
-        },
-    }
+  ],
+  get: {
+    list: function () {
+      return 'Processes.get.list();';
+    },
+    process: function (process_id) {
+      return `Processes.get.process(${process_id})`;
+    },
+  },
+  set: {
+    process: function () {
+      return 'Processes.set.process()';
+    },
+  },
 };
 
 // Пример обьекта процесса с временными отрезками
@@ -74,17 +76,17 @@ let Processes = {
 // }
 
 let Process = {
-    create: function() {
-        return {
-            name: "Раны в ДМ",
-            description: "Раны в ДМ за понедельник",
-            timeIntervals: [],
-            time_created: 0,
-            time_modified: 0,
-            total: {
-                work_time: 0,
-                all_time: 0,
-            },
-        };
-    },
+  create: function () {
+    return {
+      name: 'Раны в ДМ',
+      description: 'Раны в ДМ за понедельник',
+      timeIntervals: [],
+      time_created: 0,
+      time_modified: 0,
+      total: {
+        work_time: 0,
+        all_time: 0,
+      },
+    };
+  },
 };
